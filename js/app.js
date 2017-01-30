7fe7cfc7114e2ee80d3701c96b4cecd31a630484
jmt75200 <joelle@pcgamerzhawaii.com>
2017-01-29 22:40:55 -1000

    Adding transform onclick
    Wrote psuedo code for matching and adding point.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+function turnCard(card) {
+  document.getElementById(card).style.transform = 'rotateY(180deg)';
+
+}
+  // limit clicks to two
+
+  // if limit is reached
+
+    // AND contents match add style opacity: 0.5;
+
+      // add one point to score
+
+    // else close open cards and open next card
